e19397e3f4ac7e92c08f26c3f2ff2b49c0b59a61
mehmetkocerr <46313560+mehmetkocerr@users.noreply.github.com>
2020-08-13 22:20:23 +0300

    heroku deploy settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,12 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
 module.exports = {
-  module: {
-    loaders: [
-      {
-        test: /\.vue$/,
-        loader: 'vue'
+  // ...the webpack configuration
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      eslint: {
+        files: './src/**/*.{ts,tsx,js,jsx}', // required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
       },
-      {
-        test: /\.s[a|c]ss$/,
-        loader: 'style!css!sass'
-      }
-    ]
-  },
-  vue: {
-    loaders: {
-      scss: 'style!css!sass'
-    }
-  }
+    }),
+  ],
 }
